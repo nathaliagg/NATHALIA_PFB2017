80f6af99eadbbe7cf4205daffdb7cf51ab182fa3
ngrafgr <ngrafgr@okstate.edu>
2017-10-19 21:16:12 -0400

    another commit 10/19

---
+++ Python5/R-complement.py
@@ -0,0 +1,23 @@
+## could not do it!
+
+#!/usr/bin/env python3
+import sys
+
+# This is a script that returns both the reverse complement of 
+# DNA sequences in a file.
+
+#with open(sys.argv[1]) as file_one:
+#	for line in file_one:
+#		 = key.replace('A','t')
+#	group = key.replace('T','a')
+#	key = key.replace('C','g')
+#	key = key.replace('G','c')
+#	key = key.upper()
+#	key = key[::-1]
+#	
+#print(key)
+
+#for key,value in fasta:
+	#fasta[key] = fasta[key]+"  This is reverse complement  "+sequence
+
+
